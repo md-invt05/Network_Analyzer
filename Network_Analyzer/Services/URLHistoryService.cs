d48652cb761be391f3ef6bcc226637050ae031ff
Maxim <maks.degtearyov25@gmail.com>
2025-04-05 00:56:10 +0300

    Реализована функция истории запросов к URL

--- Network_Analyzer/Services/URLHistoryService.cs
+++ Network_Analyzer/Services/URLHistoryService.cs
@@ -1,6 +1,16 @@
+using System.Collections.ObjectModel;
+
 namespace Network_Analyzer.Services;
 
 public class URLHistoryService
 {
+    public ObservableCollection<string> History { get; } = new ObservableCollection<string>();
     
+    public void AddUrlToHistory(string url)
+    {
+        if (!string.IsNullOrWhiteSpace(url))
+        {
+            History.Insert(0, url);
+        }
+    }
 }
