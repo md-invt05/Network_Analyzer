e24c4a80a27e3ca75f75f6436a80b29ade1d2607
Maxim <maks.degtearyov25@gmail.com>
2025-04-05 00:52:03 +0300

    Реализована работа с IP в Adress Service

--- Network_Analyzer/Services/AddresService.cs
+++ Network_Analyzer/Services/AddresService.cs
@@ -1,6 +1,19 @@
+using System.Net;
+
 namespace Network_Analyzer.Services;
 
 public class AddresService
 {
-    
+    public string GetAddressType(string address)
+    {
+        if (IPAddress.TryParse(address, out var ipAddress))
+        {
+            if (ipAddress.Equals(IPAddress.Loopback))
+            {
+                return "Loopback";
+            }
+            return "Public";
+        }
+        return "Invalid IP Address";
+    }
 }
