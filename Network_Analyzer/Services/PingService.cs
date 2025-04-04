5f9bfbaae2fcf952728d62e80c27ca61cd1f3989
Maxim <maks.degtearyov25@gmail.com>
2025-04-05 00:43:33 +0300

    Реализация RelayCommand и PingService

--- Network_Analyzer/Services/PingService.cs
+++ Network_Analyzer/Services/PingService.cs
@@ -1,6 +1,22 @@
+using System.Net.NetworkInformation;
+
 namespace Network_Analyzer.Services;
 
 public class PingService
 {
-    
+    public async Task<bool> PingHostAsync(string host)
+    {
+        using var ping = new Ping();
+        try
+        {
+            PingReply reply = await ping.SendPingAsync(host);
+            return reply.Status == IPStatus.Success;
+        }
+        catch (Exception ex)
+        {
+            // Логируем/выводим ошибку
+            Console.WriteLine($"Ping error: {ex.Message}");
+            return false;
+        }
+    }
 }
--- Network_Analyzer/Services/RelayCommandService.cs
+++ Network_Analyzer/Services/RelayCommandService.cs
@@ -1,6 +1,31 @@
+using System.Windows.Input;
+
 namespace Network_Analyzer.Services;
 
-public class RelayCommandService
+public class RelayCommandService : ICommand
 {
-    
+    private readonly Action<object> _execute;
+    private readonly Func<object, bool> _canExecute;
+
+    public RelayCommandService(Action<object> execute, Func<object, bool> canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return _canExecute == null || _canExecute(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        _execute(parameter);
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add    => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
 }
